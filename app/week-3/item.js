9642ed34e2d653e83538d060f8d7841f701e65fe
Jeffrey-Rivera <jeffreyrivera711@gmail.com>
2024-06-04 14:25:16 -0600

    week 3

---
+++ app/week-3/item.js
@@ -0,0 +1,11 @@
+export default function Item({ name, quantity, category }) {
+  return (
+    <section>
+      <li className="bg-slate-800 rounded-md p-5 m-5 text-stone-50 w-96">
+      <p className="text-lg font-semibold">{name}</p>
+      <p>Buy {quantity} in {category} </p>
+    </li>
+    </section>
+
+    );
+}
